aa3fcbba582d4084431d02a169a5df30b742538b
reffypradana10 <reffypradana10@gmail.com>
2024-03-11 06:55:58 +0700

    Update Sort(), penambahan Kasus

--- Sort().js
+++ Sort().js
@@ -21,3 +21,37 @@ console.log(angka.sort(bandingNmr));
 
 // Cara Ringkas tanpa membuat function
 console.log(angka.sort((a, b) => a - b));
+
+// Contoh lain
+const pegawai = [
+  { name: "alex", id: 3 },
+  { name: "roshad", id: 4 },
+  { name: "hapid", id: 15 },
+  { name: "sapil", id: 1 },
+  { name: "Rephi", id: 9 },
+  { name: "Upil", id: 213 },
+];
+
+// Pegawai akan terurut sesuai id
+console.log(pegawai.sort((a, b) => a.id - b.id));
+
+// Pegawai akan terurut sesuai nama
+const a = pegawai.sort((a, b) => {
+  // Mengabaikan huruf besar dan huruf kecil
+  const na = a.name.toLocaleUpperCase();
+  const nb = b.name.toLocaleUpperCase();
+
+  // Jika na kurang dari nb berarti element pertama harus ditempatkan sebelum nama elemen kedua dalam urutan abjat
+  // contoh, jika na(alex) dan nb(roshad) maka na<nb akan mengembalikan true dan fungsi sort akan
+  // mengurutkan elemen pertama(alex) sebelum elemen kedua(roshad)
+  if (na < nb) return -1;
+
+  // Sebaliknya, jika na lebih besar darin nb, itu berarti nama elemen pertama harus ditempatkan setelah nama elemen
+  // kedua dalam urutan abjat
+  if (na > nb) return 1;
+
+  // Mengembalikan 0 jika keduanya sama
+  return 0;
+});
+
+console.log(a);
