815b8f2e24d542890051a0287c420645f9a3eb0e
reffypradana10 <reffypradana10@gmail.com>
2024-03-11 07:11:37 +0700

    Update Sort(), update kasus agar lebih ringkas

--- Sort().js
+++ Sort().js
@@ -9,18 +9,18 @@
 
 // Cara Dasar Mengurutkan Alfabet
 const alfabet = ["z", "b", "B", "A", "c"];
-console.log(alfabet.sort());
+// console.log(alfabet.sort());
 
 // Mengurutkan Angka
 const angka = [1, 2, 2, 35, 6, 8, 454, 12];
 // Akan Diurutkan berdasarkan angka didepan
-console.log(angka.sort());
+// console.log(angka.sort());
 // Mengurutkan Angka menggunakan compareFunction
 const bandingNmr = (a, b) => a - b;
-console.log(angka.sort(bandingNmr));
+// console.log(angka.sort(bandingNmr));
 
 // Cara Ringkas tanpa membuat function
-console.log(angka.sort((a, b) => a - b));
+// console.log(angka.sort((a, b) => a - b));
 
 // Contoh lain
 const pegawai = [
@@ -33,25 +33,31 @@ const pegawai = [
 ];
 
 // Pegawai akan terurut sesuai id
-console.log(pegawai.sort((a, b) => a.id - b.id));
+// console.log(pegawai.sort((a, b) => a.id - b.id));
 
 // Pegawai akan terurut sesuai nama
-const a = pegawai.sort((a, b) => {
-  // Mengabaikan huruf besar dan huruf kecil
-  const na = a.name.toLocaleUpperCase();
-  const nb = b.name.toLocaleUpperCase();
+// const a = pegawai.sort((a, b) => {
+//   // Mengabaikan huruf besar dan huruf kecil
+//   const na = a.name.toLocaleUpperCase();
+//   const nb = b.name.toLocaleUpperCase();
 
-  // Jika na kurang dari nb berarti element pertama harus ditempatkan sebelum nama elemen kedua dalam urutan abjat
-  // contoh, jika na(alex) dan nb(roshad) maka na<nb akan mengembalikan true dan fungsi sort akan
-  // mengurutkan elemen pertama(alex) sebelum elemen kedua(roshad)
-  if (na < nb) return -1;
+//   // Jika na kurang dari nb berarti element pertama harus ditempatkan sebelum nama elemen kedua dalam urutan abjat
+//   // contoh, jika na(alex) dan nb(roshad) maka na<nb akan mengembalikan true dan fungsi sort akan
+//   // mengurutkan elemen pertama(alex) sebelum elemen kedua(roshad)
+//   if (na < nb) return -1;
 
-  // Sebaliknya, jika na lebih besar darin nb, itu berarti nama elemen pertama harus ditempatkan setelah nama elemen
-  // kedua dalam urutan abjat
-  if (na > nb) return 1;
+//   // Sebaliknya, jika na lebih besar darin nb, itu berarti nama elemen pertama harus ditempatkan setelah nama elemen
+//   // kedua dalam urutan abjat
+//   if (na > nb) return 1;
 
-  // Mengembalikan 0 jika keduanya sama
-  return 0;
-});
+//   // Mengembalikan 0 jika keduanya sama
+//   return 0;
+// });
 
-console.log(a);
+// Cara yang lebih ringkas untuk mengurutkan sesuai nama
+const a = pegawai.sort((a, b) => a.name.localeCompare(b.name));
+// console.log(a);
+
+// Cara yang paling ringkas
+pegawai.sort((a, b) => a.name.localeCompare(b.name));
+console.log(pegawai);
